299bcc4f38bc7cd7600ddcf7a3da9251d212260e
Clémence Laffont <clemence.laffont_ext@michelin.com>
2020-11-16 19:37:42 +0100

    fix and remove some unusefull

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { SiglePageComponent } from './sigle-page/sigle-page.component';
+import { SinglePageComponent } from './single-page/single-page.component';
 import { LegalNoticeComponent } from './legal-notice/legal-notice.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SiglePageComponent,
+    SinglePageComponent,
     LegalNoticeComponent
   ],
   imports: [
